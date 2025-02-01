7d02a6f2a7c1668f0fbe8496b622f4e07638e232
Smit Bhamwala <smitbhamwala@gmail.com>
2025-02-02 01:53:13 +0530

    Add Meeting Card, firebase and assemblyai

---
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { uploadFile } from "@/lib/firebase";
+import { Presentation, Upload } from "lucide-react";
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+export default function MeetingCard() {
+	const [isUploading, setIsUploading] = useState(false);
+	const [progress, setProgress] = useState(0);
+	const { getRootProps, getInputProps } = useDropzone({
+		accept: {
+			audio: [".mp3", ".wav", ".m4a"]
+		},
+		multiple: false,
+		maxSize: 50 * 1024 * 1024,
+		onDrop: async (acceptedFiles) => {
+			setIsUploading(true);
+			console.log(acceptedFiles);
+			const file = acceptedFiles[0];
+			const downloadURL = await uploadFile(file, setProgress);
+			setIsUploading(false);
+		}
+	});
+
+	return (
+		<Card
+			className="col-span-2 flex flex-col items-center justify-center p-10"
+			{...getRootProps()}>
+			{!isUploading && (
+				<>
+					<Presentation className="h-10 w-10 animate-bounce" />
+					<h3 className="mt-2 text-sm font-semibold text-gray-900">
+						Create a new meeting
+					</h3>
+					<p className="mt-1 text-center text-sm text-gray-500">
+						Analyse your meeting
+						<br />
+						Powered by AI
+					</p>
+					<div className="mt-6">
+						<Button disabled={isUploading}>
+							<Upload className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden />
+							Upload Meeting
+							<input className="hidden" {...getInputProps()} />
+						</Button>
+					</div>
+				</>
+			)}
+		</Card>
+	);
+}
---
+++ src/lib/firebase.ts
@@ -0,0 +1,69 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+	getDownloadURL,
+	getStorage,
+	ref,
+	uploadBytesResumable
+} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyAIpvduLKmDCg8WYnkIiWlDUZhZvNZF-0I",
+	authDomain: "understandyourcodeai.firebaseapp.com",
+	projectId: "understandyourcodeai",
+	storageBucket: "understandyourcodeai.firebasestorage.app",
+	messagingSenderId: "348442438541",
+	appId: "1:348442438541:web:3567663778020389fd6bc4",
+	measurementId: "G-PDHXKSDKSX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFile(
+	file: File,
+	setProgress?: (progress: number) => void
+) {
+	return new Promise((resolve, reject) => {
+		try {
+			const storageRef = ref(storage, file.name);
+			const uploadTask = uploadBytesResumable(storageRef, file);
+
+			uploadTask.on(
+				"state_changed",
+				(snapshot) => {
+					const progress = Math.round(
+						(snapshot.bytesTransferred / snapshot.totalBytes) * 100
+					);
+					if (setProgress) {
+						setProgress(progress);
+					}
+					switch (snapshot.state) {
+						case "paused":
+							console.log("Upload is paused");
+							break;
+						case "running":
+							console.log("Upload is running");
+							break;
+					}
+				},
+				(error) => {
+					reject(error);
+				},
+				() => {
+					getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+						resolve(downloadUrl);
+					});
+				}
+			);
+		} catch (error) {
+			console.error(error);
+			reject(error);
+		}
+	});
+}
