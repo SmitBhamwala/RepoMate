9159d57cf2cc5768a26a1f6360ca9f2e6136f260
Smit Bhamwala <smit.bhamwala@terumobct.com>
2025-03-10 18:11:35 +0530

    Add meetings page

--- src/app/(protected)/dashboard/meeting-card.tsx
+++ src/app/(protected)/dashboard/meeting-card.tsx
@@ -7,68 +7,95 @@ import { Presentation, Upload } from "lucide-react";
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import { api } from "@/trpc/react";
+import useProject from "@/hooks/use-project";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export default function MeetingCard() {
-	const [isUploading, setIsUploading] = useState(false);
-	const [progress, setProgress] = useState(0);
-	const { getRootProps, getInputProps } = useDropzone({
-		accept: {
-			"audio/*": [".mp3", ".wav", ".m4a"]
-		},
-		multiple: false,
-		maxSize: 50 * 1024 * 1024,
-		onDrop: async (acceptedFiles) => {
-			setIsUploading(true);
-			console.log(acceptedFiles);
-			const file = acceptedFiles[0];
-			const downloadURL = await uploadFile(file, setProgress);
-			console.log(downloadURL);
+  const { project } = useProject();
+  const [isUploading, setIsUploading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+  const uploadMeeting = api.project.uploadMeeting.useMutation();
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      "audio/*": [".mp3", ".wav", ".m4a"],
+    },
+    multiple: false,
+    maxSize: 50 * 1024 * 1024,
+    onDrop: async (acceptedFiles) => {
+      if (!project) {
+        return;
+      }
+      setIsUploading(true);
+      const file = acceptedFiles[0];
+      if (!file) {
+        return;
+      }
+      const downloadURL = (await uploadFile(file, setProgress)) as string;
+      uploadMeeting.mutate(
+        {
+          projectId: project.id,
+          meetingURL: downloadURL,
+          name: file.name,
+        },
+        {
+          onSuccess: () => {
+            toast.success("Meeting uploaded successfully");
+            router.push("/meetings");
+          },
+          onError: () => {
+            toast.error("Failed to upload meeting");
+          },
+        }
+      );
 
-			setIsUploading(false);
-		}
-	});
-	console.log(progress);
+      setIsUploading(false);
+    },
+  });
 
-	return (
-		<Card
-			className="col-span-2 flex flex-col items-center justify-center p-10"
-			{...getRootProps()}>
-			{!isUploading && (
-				<>
-					<Presentation className="h-10 w-10 animate-bounce" />
-					<h3 className="mt-2 text-sm font-semibold text-gray-900">
-						Create a new meeting
-					</h3>
-					<p className="mt-1 text-center text-sm text-gray-500">
-						Analyse your meeting
-						<br />
-						Powered by AI
-					</p>
-					<div className="mt-6">
-						<Button disabled={isUploading}>
-							<Upload className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden />
-							Upload Meeting
-							<input className="hidden" {...getInputProps()} />
-						</Button>
-					</div>
-				</>
-			)}
-			{isUploading && (
-				<div className="flex items-center justify-center flex-col">
-					<CircularProgressbar
-						value={progress}
-						text={`${progress}%`}
-						className="size-20"
-						styles={buildStyles({
-							pathColor: "#2563eb",
-							textColor: "#2563eb"
-						})}
-					/>
-					<p className="text-sm text-gray-500 text-center">
-						Uploading your meeting...
-					</p>
-				</div>
-			)}
-		</Card>
-	);
+  return (
+    <Card
+      className="col-span-2 flex flex-col items-center justify-center p-10"
+      {...getRootProps()}
+    >
+      {!isUploading && (
+        <>
+          <Presentation className="h-10 w-10 animate-bounce" />
+          <h3 className="mt-2 text-sm font-semibold text-gray-900">
+            Create a new meeting
+          </h3>
+          <p className="mt-1 text-center text-sm text-gray-500">
+            Analyse your meeting
+            <br />
+            Powered by AI
+          </p>
+          <div className="mt-6">
+            <Button disabled={isUploading}>
+              <Upload className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden />
+              Upload Meeting
+              <input className="hidden" {...getInputProps()} />
+            </Button>
+          </div>
+        </>
+      )}
+      {isUploading && (
+        <div className="flex items-center justify-center flex-col">
+          <CircularProgressbar
+            value={progress}
+            text={`${progress}%`}
+            className="size-20"
+            styles={buildStyles({
+              pathColor: "#2563eb",
+              textColor: "#2563eb",
+            })}
+          />
+          <p className="text-sm text-gray-500 text-center">
+            Uploading your meeting...
+          </p>
+        </div>
+      )}
+    </Card>
+  );
 }
--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -5,104 +5,138 @@ import { pollCommits } from "@/lib/github";
 import { indexGitHubRepo } from "@/lib/github-loader";
 
 export const projectRouter = createTRPCRouter({
-	createProject: protectedProcedure
-		.input(
-			z.object({
-				name: z.string(),
-				gitHubURL: z.string(),
-				gitHubToken: z.string().optional()
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			const project = await ctx.db.project.create({
-				data: {
-					name: input.name,
-					gitHubURL: input.gitHubURL,
-					userToProjects: {
-						create: {
-							userId: ctx.user.user.id
-						}
-					}
-				}
-			});
-			await indexGitHubRepo(project.id, input.gitHubURL, input.gitHubToken);
-			await pollCommits(project.id);
-			return project;
-		}),
-	getProjects: protectedProcedure.query(async ({ ctx }) => {
-		return await ctx.db.project.findMany({
-			where: {
-				userToProjects: {
-					some: {
-						userId: ctx.user.user.id
-					}
-				}
-			}
-		});
-	}),
-	getCommits: protectedProcedure
-		.input(
-			z.object({
-				projectId: z.string()
-			})
-		)
-		.query(async ({ ctx, input }) => {
-			try {
-				await pollCommits(input.projectId);
-			} catch (error) {
-				console.error(error);
-			}
-			return await ctx.db.commits.findMany({
-				where: {
-					projectId: input.projectId
-				},
-				orderBy: { commitDate: "desc" },
-				take: 10
-			});
-		}),
-	saveAnswer: protectedProcedure
-		.input(
-			z.object({
-				question: z.string(),
-				answer: z.string(),
-				projectId: z.string(),
-				fileReferences: z.array(
-					z.object({
-						fileName: z.string(),
-						sourceCode: z.string(),
-						summary: z.string()
-					})
-				)
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			return await ctx.db.questions.create({
-				data: {
-					question: input.question,
-					answer: input.answer,
-					projectId: input.projectId,
-					fileReferences: input.fileReferences,
-					userId: ctx.user.user.id
-				}
-			});
-		}),
-	getQuestions: protectedProcedure
-		.input(
-			z.object({
-				projectId: z.string()
-			})
-		)
-		.query(async ({ ctx, input }) => {
-			return await ctx.db.questions.findMany({
-				where: {
-					projectId: input.projectId
-				},
-				include: {
-					user: true
-				},
-				orderBy: {
-					createdAt: "desc"
-				}
-			});
-		})
+  createProject: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        gitHubURL: z.string(),
+        gitHubToken: z.string().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const project = await ctx.db.project.create({
+        data: {
+          name: input.name,
+          gitHubURL: input.gitHubURL,
+          userToProjects: {
+            create: {
+              userId: ctx.user.user.id,
+            },
+          },
+        },
+      });
+      await indexGitHubRepo(project.id, input.gitHubURL, input.gitHubToken);
+      await pollCommits(project.id);
+      return project;
+    }),
+  getProjects: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.project.findMany({
+      where: {
+        userToProjects: {
+          some: {
+            userId: ctx.user.user.id,
+          },
+        },
+      },
+    });
+  }),
+  getCommits: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        await pollCommits(input.projectId);
+      } catch (error) {
+        console.error(error);
+      }
+      return await ctx.db.commits.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+        orderBy: { commitDate: "desc" },
+        take: 10,
+      });
+    }),
+  saveAnswer: protectedProcedure
+    .input(
+      z.object({
+        question: z.string(),
+        answer: z.string(),
+        projectId: z.string(),
+        fileReferences: z.array(
+          z.object({
+            fileName: z.string(),
+            sourceCode: z.string(),
+            summary: z.string(),
+          })
+        ),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.questions.create({
+        data: {
+          question: input.question,
+          answer: input.answer,
+          projectId: input.projectId,
+          fileReferences: input.fileReferences,
+          userId: ctx.user.user.id,
+        },
+      });
+    }),
+  getQuestions: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.questions.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }),
+  uploadMeeting: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+        meetingURL: z.string(),
+        name: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.meetings.create({
+        data: {
+          projectId: input.projectId,
+          meetingURL: input.meetingURL,
+          name: input.name,
+          status: "PROCESSING",
+        },
+      });
+    }),
+  getMeetings: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      return await ctx.db.meetings.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+        include: {
+          issues: true,
+        },
+      });
+    }),
 });
