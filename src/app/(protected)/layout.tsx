0789597170f664681e35b1bd4a651744c440c098
Smit Bhamwala <smit.bhamwala@terumobct.com>
2025-03-12 01:20:22 +0530

    Add profile avatar

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -4,6 +4,8 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import AppSidebar from "./app-sidebar";
 import { SessionProvider, signOut, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export default function SidebarLayout({
 	children
@@ -17,21 +19,46 @@ export default function SidebarLayout({
 			<SidebarProvider>
 				<AppSidebar />
 				<main className="w-full m-2">
-					<div className="flex items-center justify-end gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4">
-						{/* <SearchBar /> */}
-						<p>{session?.user.name}</p>
-						<Button type="submit" onClick={() => signOut()}>
-							Logout
-						</Button>
-						{/* <div className="ml-auto"></div> */}
-						{/* <UserButton /> */}
-					</div>
-					<div className="h-4"></div>
-					{/* main content */}
-					<div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-6rem)] p-4">
-						{children}
-					</div>
-				</main>
+          <div className="flex items-center justify-end gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4">
+            {/* <SearchBar /> */}
+            <p>{session?.user.name}</p>
+
+            <div className="ml-auto"></div>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="outline-none">
+                <Avatar>
+                  <AvatarImage
+                    src={
+                      session?.user.image ||
+                      "https://github.com/shadcn.png" ||
+                      ""
+                    }
+                  />
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel className="flex justify-center items-center">
+                  {session?.user.name}
+                </DropdownMenuLabel>
+                <DropdownMenuItem>
+                  <Button
+                    className="w-full"
+                    type="submit"
+                    onClick={() => signOut()}
+                  >
+                    Logout
+                  </Button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <div className="h-4"></div>
+
+          {/* main content */}
+          <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-6rem)] p-4">
+            {children}
+          </div>
+        </main>
 			</SidebarProvider>
 		</SessionProvider>
 	);
