56cb872b47560f312beafcdcfdb39508426be7f7
Smit Bhamwala <smit.bhamwala@terumobct.com>
2025-03-15 02:22:25 +0530

    Fix dashboard hydration error

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -11,40 +11,44 @@ import InviteButton from "./invite-button";
 import TeamMembers from "./team-members";
 
 export default function DashboardPage() {
-	const { project, activeProjectId } = useProject();
+  const { project } = useProject();
 
-	if (!activeProjectId) {
-		return <div className="flex items-center justify-center">Please select a project from the sidebar or create a new project</div>;
-	}
+  return (
+    <>
+      {project ? (
+        <div>
+          <div className="flex items-center justify-between flex-wrap gap-y-4">
+            <Link
+              href={project?.gitHubURL ?? ""}
+              target="_blank"
+              className="flex items-center gap-1 text-sm font-medium text-white w-fit rounded-md bg-primary px-4 py-3"
+            >
+              <Github className="size-5 flex-shrink-0" />
+              <span>{project?.name}&apos;s Source Code</span>
+            </Link>
 
-	return (
-		<div>
-			<div className="flex items-center justify-between flex-wrap gap-y-4">
-				<Link
-					href={project?.gitHubURL ?? ""}
-					target="_blank"
-					className="flex items-center gap-1 text-sm font-medium text-white w-fit rounded-md bg-primary px-4 py-3"
-				>
-					<Github className="size-5 flex-shrink-0" />
-					<span>{project?.name}&apos;s Source Code</span>
-				</Link>
+            <div className="h-4"></div>
 
-				<div className="h-4"></div>
-
-				<div className="flex items-center gap-4">
-					<TeamMembers />
-					<InviteButton /> 
-					<ArchiveButton />
-				</div>
-			</div>
-			<div className="mt-4">
-				<div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
-					<AskQuestionCard />
-					<MeetingCard />
-				</div>
-			</div>
-			<div className="mt-8"></div>
-			{project && <CommitLog />}
-		</div>
-	);
+            <div className="flex items-center gap-4">
+              <TeamMembers />
+              <InviteButton />
+              <ArchiveButton />
+            </div>
+          </div>
+          <div className="mt-4">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+              <AskQuestionCard />
+              <MeetingCard />
+            </div>
+          </div>
+          <div className="mt-8"></div>
+          {project && <CommitLog />}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center">
+          Please select a project from the sidebar or create a new project
+        </div>
+      )}
+    </>
+  );
 }
